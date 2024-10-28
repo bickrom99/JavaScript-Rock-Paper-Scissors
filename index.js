7e3a6c7716596cb8260efbe8c9b5f8f14e81462c
bickrom99 <bickromsen99@gmail.com>
2024-10-28 16:59:09 +0600

    update

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-
+let result_id = document.querySelector('#result_id');
 // main game ression --> 0-1 between and 0-1/3-1/2-1 game
 
 
@@ -22,12 +21,96 @@ function RockButton(){
 
     let reult = '';
     if(computerMuve === 'rock'){
-        reult = 'tie'
+        reult = 'Tie'
+        result_id.textContent = 'Tie🤘'
     }else if(computerMuve === 'paper') {
-        reult = 'you loss'
+        reult = ' You loss'
+        result_id.textContent = 'You Loss😔'
     }else if(computerMuve === 'scissors'){
-        reult = 'you win'
+        reult = ' You win'
+        result_id.textContent = 'You Win🏆'
     }
-    console.log(computerMuve);
-    console.log(reult);
+    // Computer result alert
+
+    alert(`You Pick rock. Computer Pick ${computerMuve}. ${reult}`);
+}
+
+
+
+
+// Paper-button work here
+function PaperButton(){
+    const rendomNumber = Math.random();
+    let computerMuve = '';
+
+    if(rendomNumber >=0 && rendomNumber <1/3){
+        computerMuve = "rock";
+
+    }else if(rendomNumber >= 1/3 && rendomNumber < 1/2 ) {
+        computerMuve = "paper"
+
+    }else if(rendomNumber >= 1/2 && rendomNumber < 1 ) {
+        computerMuve = "scissors"
+    }
+    
+    // Game result here
+
+    let reult = '';
+    if(computerMuve === 'rock'){
+        reult = ' You win';
+        result_id.textContent = 'You Win🏆'
+    }else if(computerMuve === 'paper') {
+        reult = 'Tie';
+        result_id.textContent = 'Tie🤘'
+    }else if(computerMuve === 'scissors'){
+        reult = ' You loss';
+        result_id.textContent = 'You Loss😔'
+    }
+    
+    // Computer result alert
+    alert(`You Pick paper. Computer Pick ${computerMuve}. ${reult}`);
+}
+
+
+
+
+// Scissors-button work here
+function ScissorsButton(){
+    const rendomNumber = Math.random();
+    let computerMuve = '';
+
+    if(rendomNumber >=0 && rendomNumber <1/3){
+        computerMuve = "rock";
+
+    }else if(rendomNumber >= 1/3 && rendomNumber < 1/2 ) {
+        computerMuve = "paper"
+
+    }else if(rendomNumber >= 1/2 && rendomNumber < 1 ) {
+        computerMuve = "scissors"
+    }
+    
+    // Game result here
+
+    let reult = '';
+    if(computerMuve === 'rock'){
+        reult = ' You loss';
+        result_id.textContent = 'You Loss😔'
+    }else if(computerMuve === 'paper') {
+        reult = 'You win';
+        result_id.textContent = 'You Win🏆'
+    }else if(computerMuve === 'scissors'){
+        reult = 'Tie';
+        result_id.textContent = 'Tie🤘'
+    }
+    
+    // Computer result alert
+    alert(`You Pick scissors. Computer Pick ${computerMuve}. ${reult}`);
+}
+
+
+
+
+// Reset Button here
+function ResetButton(){
+    document.querySelector('#All_Result').style.display = 'none';
 }
